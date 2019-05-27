f5084052924d847e5c1ab72d4cd70b8b5ef9f7c5
jeepo <jeepeo55@gmail.com>
2019-05-27 13:01:42 +0530

    Cooked /unpin and minor fix for /pin

--- sophie_bot/modules/misc.py
+++ sophie_bot/modules/misc.py
@@ -34,6 +34,9 @@ async def id(event):
 @user_admin_dec
 async def pinMessage(event):
     tagged_message = await event.get_reply_message()
+    if not tagged_message:
+        await event.reply(get_string('misc', 'no_reply_msg', event.chat_id))
+        return
     msg_2_pin = tagged_message.id
     base_url = 'https://api.telegram.org/bot{}/pinChatMessage'.format(TOKEN)
     data = {'chat_id': event.chat_id, 'message_id': msg_2_pin}
@@ -49,3 +52,11 @@ async def pinMessage(event):
     requests.post(base_url, data)  # TODO: catch error | Wait for telethon support pin :D
 # CatchError=bot API throw error{error: chat_not_modified}[not in shell] if pin on already pined msg
     await event.reply(get_string('misc', 'pinned_success', event.chat_id))
+
+
+@decorator.command("unpin")
+async def unpinMessage(event):
+    base_url = 'https://api.telegram.org/bot{}/unpinChatMessage'.format(TOKEN)
+    data = {'chat_id': event.chat_id}
+    requests.post(base_url, data)  # TODO: CatchError
+    await event.reply(get_string('misc', 'unpin_success', event.chat_id))
